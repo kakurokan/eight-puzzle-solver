15e38042b9b832573258e4e5b58700532f1e5335
MintermMaster <acrismedemanuel@gmail.com>
2026-09-21 10:10:53 +0100

    Add JUnit 5.8.1 and comprehensive unit tests for `Board` and `BestFirst` classes

--- test/PuzzleUnitTests.java
+++ test/PuzzleUnitTests.java
@@ -1,5 +1,8 @@
 import java.io.StringWriter;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +30,117 @@ public class PuzzleUnitTests {
         pw.println("67 ");
         assertEquals(writer.toString(), b.toString());
         pw.close();
+    }
+
+    @Test
+    public void equals(){
+        Board b = new Board("876543210");
+        Object o = new Board("876543210");
+        assertEquals(b, o);
+    }
+
+    @Test
+    public void diferentes(){
+        Board b = new Board("678543210");
+        Object o = new Board("876543210");
+        assertNotEquals(b, o);
+    }
+
+    @Test
+    public void identidade(){
+        Board b = new Board("876543210");
+        assertEquals(b, b);
+    }
+
+    @Test
+    public void nulo(){
+        Board b = new Board("678543210");
+        assertNotEquals(null, b);
+    }
+
+    @Test
+    public void Clone(){
+        Board a = new Board("023145678");
+        Board b = a.clone();
+        assertEquals(a,b);
+    }
+
+    @Test
+    public void codigo_hash(){
+        Board a = new Board("023145678");
+        Board b = a.clone();
+        assertEquals(a.hashCode(),b.hashCode());
+    }
+
+    @Test
+    public void Teste_children_2_movimentos(){
+        Board b = new Board("012345678");
+        List<Ilayout> filhos = b.children();
+        assertEquals(2,filhos.size());
+
+        Board c = new Board("102345678");
+        Board d = new Board("312045678");
+        assertTrue(filhos.contains(c));
+        assertTrue(filhos.contains(d));
+    }
+
+    @Test
+    public void Teste_children_3_movimentos(){
+        Board b = new Board("102345678");
+        List<Ilayout> filhos = b.children();
+        assertEquals(3,filhos.size());
+    }
+
+    @Test
+    public void Teste_children_4_movimentos(){
+        Board b = new Board("123405678");
+        List<Ilayout> filhos = b.children();
+        assertEquals(4,filhos.size());
+    }
+
+    @Test
+    public void test_solve_1(){
+        int contador = 0;
+        Board inicial = new Board("123456078");
+        Board objectivo = new Board("123456780");
+
+        BestFirst best = new BestFirst();
+        Iterator<BestFirst.State> res = best.solve(inicial,objectivo);
+
+        assertNotNull(res);
+        while(res.hasNext()){
+            contador++;
+            res.next();
+        }
+        assertEquals(3,contador);
+    }
+
+    @Test
+    public void test_solve_2(){
+        int contador = 0;
+        Board inicial = new Board("123456780");
+        Board objectivo = new Board("123456780");
+
+        BestFirst best = new BestFirst();
+        Iterator<BestFirst.State> res = best.solve(inicial,objectivo);
+
+        assertNotNull(res);
+        while(res.hasNext()){
+            contador++;
+            res.next();
+        }
+        assertEquals(1,contador);
+    }
+
+    @Test
+    public void test_solve_3(){
+        Board inicial = new Board("123456870");
+        Board objectivo = new Board("123456780");
+
+        BestFirst best = new BestFirst();
+        Iterator<BestFirst.State> res = best.solve(inicial,objectivo);
+
+        assertNull(res);
     }
 
     // TODO: Add tests for the remaining Board methods and for solve.
